e36307fe6f1659a0f216c7058e2dcc9a2154c39a
MidNightRoad <bergerpierre09@gmail.com>
2022-03-17 18:45:38 +0100

    add effect for title and fix bug for scrolling opacity

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const title = document.querySelector('.titlenav');
 
 window.addEventListener('scroll', function(){
     var scroll = this.scrollY;
-    if (scroll > 260){
-    title.style.visibility = 'visible';}
+    console.log(scroll)
+    if (scroll > 400){
+    title.style.opacity = `${scroll - 400}%`;}
+    else{
+        title.style.opacity = `0%`;
+    }
 })
