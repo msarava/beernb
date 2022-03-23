9fe352750666d8948a65e2450f616bcf42772a65
Lucas-jimenez-dev <pro.lucasjimenez@gmail.com#Downpower2302!Lucas-jimenez-devgit config --global user.email pro.lucasjimenez@gmail.comgit config --global user.email pro.lucasjimenez@gmail.com#>
2022-03-23 11:51:56 +0100

    j'ai sauvegardé mon travail dans card

---
+++ home.js
@@ -0,0 +1,11 @@
+const myCardClassSaintCyprien = document.querySelector('.card-saint-cyprien')
+
+myCardClassSaintCyprien.addEventListener('click', event => {
+    window.open("http://www.google.com", "_self")
+});
+
+const myCardClassCarmes = document.querySelector('.card-carmes')
+
+myCardClassCarmes.addEventListener('click', event => {
+    window.open("#", "_self")
+});
