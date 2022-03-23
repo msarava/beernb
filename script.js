a068428ee766b79d07d2a731ea0a5841cc3a783f
MidNightRoad <bergerpierre09@gmail.com>
2022-03-23 14:17:20 +0100

    rainbow mode

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const title = document.querySelector('.titlenav');
 const containerImage = document.querySelector('.card:hover');
 window.addEventListener('scroll', function(){
-    var scroll = this.scrollY;
-    
+    var scroll = this.scrollY; 
     if (scroll > 400){
     title.style.opacity = `${scroll - 400}%`;}
     else{
@@ -12,10 +11,11 @@ window.addEventListener('scroll', function(){
 const btnDarkMode = document.querySelector('#btn-darkmode')
 const cardsTitle = document.querySelectorAll('.card-title')
 const cardsBtn = document.querySelectorAll('.button-card')
-
+const burgerBg = document.querySelector('.menu__box')
 btnDarkMode.addEventListener('click', function(){
     var element = document.body;
     element.classList.toggle("dark-mode");
+    burgerBg.classList.toggle('dark')
     for (let title of cardsTitle) {
         title.classList.toggle('dark');
     }
@@ -24,3 +24,14 @@ btnDarkMode.addEventListener('click', function(){
     }
    
 });
+
+const btnRainbow = document.querySelector('#btn-rainbowmode');
+btnRainbow.addEventListener('click', function(){
+    burgerBg.classList.toggle('wrapper');
+    for (let title of cardsTitle) {
+        title.classList.toggle('wrapper');
+    }
+    for (let btn of cardsBtn) {
+        btn.classList.toggle('wrapper');
+    }
+})
