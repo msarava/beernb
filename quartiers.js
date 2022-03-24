4d9e2a412c2d783067e37a4bcbe906629f602f1d
MidNightRoad <bergerpierre09@gmail.com>
2022-03-24 13:39:10 +0100

    fix link

--- quartiers.js
+++ quartiers.js
@@ -39,3 +39,23 @@ for(let myClassCard of myClassCards){
         window.open(url, "_self")
         })
 } 
+const quartiersBtn = document.querySelector("#dropbtn")
+const menuBox =document.querySelector('.menu__box');
+const quartierLi = document.querySelector('.dropdown_quartier')
+const quartierA = document.querySelectorAll('.a-quartier')
+const font = document.querySelector('#font')
+let click = 0;
+quartiersBtn.addEventListener('click', function(){
+    click++
+    quartierLi.classList.toggle('dropdown_quartier_visible');
+    var classes = font.classList;
+    if (click % 2 > 0){
+    classes.replace("fa-hand-point-down", "fa-hand-point-up");
+    }else {
+    classes.replace("fa-hand-point-up", "fa-hand-point-down");
+    }
+   for (let a of quartierA){
+    a.classList.toggle('a-quartier-visible')
+   }
+    
+})
